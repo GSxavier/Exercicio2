711569d3b407e016d2d715699ea9d4012afedb06
Gustavo Xavier <mineirinhotico02@gmail.com>
2019-11-21 17:20:51 -0300

    ops

--- McBanaldsMVC/Models/Pedido.cs
+++ McBanaldsMVC/Models/Pedido.cs
@@ -9,6 +9,12 @@ namespace McBanaldsMVC.Models
         public Shake Shake {get;set;}
         public DateTime DataDoPedido {get; set;}
         public double PrecoTotal {get; set;}
+        public Pedido ()
+        {
+            this.Cliente = new Cliente();
+            this.Hamburguer = new Hamburguer();
+            this.Shake = new Shake();
+        }
         
     }
 }
